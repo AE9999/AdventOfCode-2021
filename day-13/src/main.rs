95a19fcf3152d8fa6e7319d36e18258931ea7b3d
ae <ae>
2021-12-13 12:53:51 +0100

    [minior] some code cleaning for day 13

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -3,21 +3,13 @@ use std::io::{self, BufReader, BufRead};
 use std::env;
 use std::collections::{HashSet, VecDeque};
 
-#[derive(Hash)]
-#[derive(PartialEq)]
-#[derive(Eq)]
-#[derive(Copy, Clone)]
-#[derive(Debug)]
+#[derive(Hash, PartialEq, Eq, Copy, Clone, Debug)]
 struct Point {
     x: i32,
     y: i32
 }
 
-#[derive(Hash)]
-#[derive(PartialEq)]
-#[derive(Eq)]
-#[derive(Copy, Clone)]
-#[derive(Debug)]
+#[derive(Hash, PartialEq, Eq, Copy, Clone, Debug)]
 struct Fold {
     axis: char,
     line: i32
@@ -31,26 +23,22 @@ struct Origami {
 
 impl Origami {
 
-    fn height(&self) -> usize {
-        (self.points.iter().map(|point|point.y).max().unwrap() + 1) as usize
-    }
+    fn height(&self) -> usize { (self.points.iter().map(|point|point.y).max().unwrap() + 1) as usize }
 
-    fn widht(&self) -> usize {
-        (self.points.iter().map(|point|point.x).max().unwrap() + 1) as usize
-    }
+    fn widht(&self) -> usize { (self.points.iter().map(|point|point.x).max().unwrap() + 1) as usize }
 
     fn fold(&mut self) {
         let fold = self.folds.pop_front().unwrap();
         let next_points: HashSet<Point> = self.points.iter().map(|point| {
             if fold.axis == 'x' {
-                let newx = if point.x < fold.line { point.x }
-                                else { fold.line - (point.x - fold.line) };
-                Point { x:  newx, y: point.y }
+                Point { x:  if point.x < fold.line { point.x }
+                            else { fold.line - (point.x - fold.line) },
+                        y: point.y }
             }
             else {
-                let newy = if point.y < fold.line { point.y }
-                                else { fold.line - (point.y - fold.line) };
-                Point { x: point.x, y: newy }
+                Point { x: point.x,
+                        y: if point.y < fold.line { point.y }
+                           else { fold.line - (point.y - fold.line) } }
             }
         }).collect();
         self.points = next_points;
@@ -63,7 +51,6 @@ impl Origami {
     }
 }
 
-
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let input = &args[1];
@@ -71,9 +58,7 @@ fn main() -> io::Result<()> {
     origami.fold();
     println!("{:?} many dots are visible after completing just the first fold instruction on your transparent paper ..",
              origami.points.len());
-    while !origami.folds.is_empty() {
-        origami.fold();
-    }
+    while !origami.folds.is_empty() { origami.fold(); }
     origami.print();
     Ok(())
 }
@@ -81,27 +66,24 @@ fn main() -> io::Result<()> {
 fn read_lines(filename: &String) -> io::Result<Origami> {
     let file_in = File::open(filename)?;
     let file_reader = BufReader::new(file_in);
-    let lines: Vec<String> = file_reader.lines().map(|x| x.unwrap()).collect();
     let mut points: HashSet<Point> = HashSet::new();
     let mut folds: VecDeque<Fold> = VecDeque::new();
-    let mut reading_points = true;
-    lines.iter().for_each(|line| {
-        if line.is_empty() {
-            reading_points = false;
-        } else {
-            if line.starts_with("fold along ") {
-                let mut split =  line["fold along ".len()..].split("=");
-                folds.push_back(Fold {
-                    axis: split.next().unwrap().chars().next().unwrap(),
-                    line: split.next().unwrap().parse::<i32>().unwrap()
-                })
-            } else {
-                let mut split = line.split(',');
-                points.insert(Point { x: split.next().unwrap().parse::<i32>().unwrap(),
-                                         y: split.next().unwrap().parse::<i32>().unwrap() } );
-            }
-        }
 
+    file_reader.lines().map(|x| x.unwrap()).for_each(|line| {
+        if line.is_empty() { return }
+        if line.starts_with("fold along ") {
+            let mut split = line["fold along ".len()..].split("=");
+            folds.push_back(Fold {
+                axis: split.next().unwrap().chars().next().unwrap(),
+                line: split.next().unwrap().parse::<i32>().unwrap()
+            })
+        } else {
+            let mut split = line.split(',');
+            points.insert(Point {
+                x: split.next().unwrap().parse::<i32>().unwrap(),
+                y: split.next().unwrap().parse::<i32>().unwrap()
+            });
+        }
     });
     Ok(Origami { points, folds })
 }
