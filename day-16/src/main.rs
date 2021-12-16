c17b0a86e167da3718950046ad40affae27cfc3c
ae <ae>
2021-12-16 09:49:08 +0100

    [minor] implements day 16

--- day-16/src/main.rs
+++ day-16/src/main.rs
@@ -2,53 +2,75 @@ use std::io::{self, BufReader, BufRead};
 use std::env;
 use std::fs::File;
 
+
+struct Node {
+    version: usize,
+    type_id: usize,
+    value: Option<usize>,
+    children: Vec<Node>,
+}
+
+impl Node {
+    fn sum_version(&self) -> usize {
+        self.version + self.children.iter()
+                                    .map(|child| child.sum_version())
+                                    .fold(0, |acc,x| acc + x)
+    }
+}
+
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let input = &args[1];
-    let lines = read_lines(input).unwrap();
-    lines.iter().for_each(|x|{
-        let decoded = decode(x);
-        let version = usize::from_str_radix(decoded[0..3].as_ref(), 2).unwrap();
-        let type_id = usize::from_str_radix(decoded[3..6].as_ref(), 2).unwrap();
-        if version == 4 { decode_literal_value(decoded[6..].as_ref()); }
-    });
-
-    println!("{:?} do you get if you add up the version numbers in all packets?", 0);
-
+    let line = read_lines(input).unwrap();
+    let (tree, _) = parse(decode(&line).as_str());
+    println!("{:?} do you get if you add up the version numbers in all packets?", tree.sum_version());
     Ok(())
 }
 
-fn decode_literal_value(decoded: &str) -> usize {
-    let result: String = String::new();
+fn parse(decoded: &str) -> (Node, usize) {
+    let version = usize::from_str_radix(decoded[0..3].as_ref(), 2).unwrap();
+    let type_id = usize::from_str_radix(decoded[3..6].as_ref(), 2).unwrap();
+    let mut value: Option<usize> = None;
+    if type_id == 4 {
+        let (value_, offset_) = decode_literal_value(decoded[6..].as_ref());
+        value = Option::from(value_);
+        let node = Node { version, type_id, value, children: Vec::new() };
+        (node, 6 + offset_)
+    } else {
+        let length_type_id: usize =  usize::from_str_radix(decoded[6..7].as_ref(), 10).unwrap();
+        let mut children: Vec<Node> = Vec::new();
+        let mut parsed_bits = 0;
+        if length_type_id == 0 {
+            let total_length_in_bits = usize::from_str_radix(decoded[7..22].as_ref(), 2).unwrap();
+            while parsed_bits < total_length_in_bits {
+                let (child, offset) = parse(decoded[22+parsed_bits..].as_ref());
+                children.push(child);
+                parsed_bits += offset;
+            }
+            if parsed_bits > total_length_in_bits { panic!("Unexpected behavior") }
+            (Node { version, type_id, value, children}, 22+parsed_bits)
+        } else {
+            let number_of_sub_packets = usize::from_str_radix(decoded[7..18].as_ref(), 2).unwrap();
+            (0..number_of_sub_packets).for_each(|_x|{
+                let (child, offset) = parse(decoded[18+parsed_bits..].as_ref());
+                children.push(child);
+                parsed_bits += offset;
+            });
+            (Node { version, type_id, value, children}, 18+parsed_bits)
+        }
+    }
+}
+
+fn decode_literal_value(decoded: &str) -> (usize, usize) {
+    let mut result: String = String::new();
     let mut offset = 0 ;
     loop {
         let group_bit = usize::from_str_radix(decoded[offset..offset + 1].as_ref(), 10).unwrap();
-        usize::from_str_radix(decoded[offset+1..offset+5].as_ref(), 2).unwrap();
-        if  group_bit == 1 { break }
-        offset += 5
+        decoded[offset+1..offset+5].chars().for_each(|x|result.push(x));
+        offset += 5;
+        if  group_bit == 0 { break }
     }
-    usize::from_str_radix(result.as_str(), 10).unwrap()
-}
-
-fn parse_lenght(decoded: &str) -> Vec<usize> {
-    let r : Vec<usize> = Vec::new();
-    r
-}
-
-fn parse_sub_packets(decoded: &str) -> (Vec<usize>, usize) {
-    let r : Vec<usize> = Vec::new();
-    (r, 0)
-}
-
-fn parse_operator_packet(decoded: &str) {
-    let length_type_id: usize =  usize::from_str_radix(decoded[0..1].as_ref(), 10).unwrap();
-    if length_type_id == 0 {
-        let total_length_in_bits = usize::from_str_radix(decoded[1..16].as_ref(), 2).unwrap();
-
-
-    } else {
-        let number_of_sub_packets = usize::from_str_radix(decoded[1..12].as_ref(), 2).unwrap();
-    }
+    (usize::from_str_radix(result.as_str(), 2).unwrap(), offset)
 }
 
 fn decode(value: &String) ->  String {
@@ -82,9 +104,9 @@ fn expand(c: &char) -> &'static str {
     }
 }
 
-fn read_lines(filename: &String) -> io::Result<Vec<String>> {
+fn read_lines(filename: &String) -> io::Result<String> {
     let file_in = File::open(filename)?;
-    let file_reader = BufReader::new(file_in);
-    Ok(file_reader.lines().filter_map(io::Result::ok).collect())
+    let  file_reader = BufReader::new(file_in);
+    Ok(file_reader.lines().next().unwrap().unwrap())
 }
 
